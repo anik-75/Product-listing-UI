8c70c3c37829246e0c59327e21f74e40653a4040
Anik <64964667+anik-75@users.noreply.github.com>
2024-03-21 16:19:13 +0530

    feat: Implemented Bookmark functionality

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const listContainer = document.querySelector(".list-container");
 const sortSelect = document.querySelector("#sort");
 const filterTags = document.querySelector(".filter-tags");
 const clearTagsBtn = document.querySelector(".clear");
+const bookmark = document.querySelector(".bookmark-container");
+let showBookmark = false;
 
 const url =
   "https://api.freeapi.app/api/v1/public/randomproducts?page=1&limit=20&inc=category%2Cprice%2Cthumbnail%2Cimages%2Ctitle%2Cid";
@@ -18,12 +20,28 @@ async function getData(url) {
 getData(url);
 
 function generateCard(obj) {
-  if (obj.title && obj.title.length > 12) {
-    let truncatedStr = obj.title.substring(0, 12);
-    let lastSpaceIndex = truncatedStr.lastIndexOf(" ");
-    obj.title = truncatedStr.substring(0, lastSpaceIndex) + "...";
-  }
-  listContainer.innerHTML += `
+  if (obj !== null) {
+    if (obj.title && obj.title.length > 12) {
+      let truncatedStr = obj.title.substring(0, 12);
+      let lastSpaceIndex = truncatedStr.lastIndexOf(" ");
+      obj.title = truncatedStr.substring(0, lastSpaceIndex) + "...";
+    }
+
+    let btnText = "Add to Fav";
+    let btnAction = "ADD";
+    let bookmarkProducts = localStorage.getItem("Products") || [];
+    if (typeof bookmarkProducts === "string") {
+      bookmarkProducts = JSON.parse(bookmarkProducts);
+      const index = bookmarkProducts.findIndex(
+        (product) => Number(product.id) === Number(obj.id)
+      );
+      if (index !== -1) {
+        btnText = "Remove";
+        btnAction = "REMOVE";
+      }
+    }
+
+    listContainer.innerHTML += `
       <div class="card" >
         <span class="card-header">
           <img src=${obj.thumbnail} alt=${obj.title}/>
@@ -33,10 +51,13 @@ function generateCard(obj) {
           <h3 class='title'>${obj?.title}</h3>
           <p class='category'>#${obj?.category.toUpperCase()}</p>
           <p class='price'>Price: $${obj.price}</p>
-          <button class="fav-btn">Add To Fav</button>
+          <button data-pid=${
+            obj.id
+          } data-action=${btnAction} class="fav-btn">${btnText}</button>
         </div>
       </div>
   `;
+  }
 }
 
 function generateCardList(data) {
@@ -79,6 +100,41 @@ listContainer.addEventListener("click", (e) => {
     generateCardList(data);
     filterTags.textContent = `#${category}`;
     clearTagsBtn.style.display = "block";
+  } else if (e.target.tagName === "BUTTON") {
+    const productId = Number(e.target.dataset.pid);
+    const action = e.target.dataset.action;
+    const product = products.find((product) => product.id === productId);
+    let bookmarkProducts = localStorage.getItem("Products") || [];
+
+    if (action === "ADD") {
+      // Add Products to local Storage
+      if (typeof bookmarkProducts === "string") {
+        bookmarkProducts = JSON.parse(bookmarkProducts);
+        let index = bookmarkProducts.findIndex(
+          (product) => Number(product.id) === Number(productId)
+        );
+        if (index === -1) {
+          bookmarkProducts.push(product);
+        }
+      } else {
+        bookmarkProducts.push(product);
+      }
+      e.target.dataset.action = "REMOVE";
+      e.target.textContent = "Remove";
+    } else if (action === "REMOVE") {
+      if (product && typeof bookmarkProducts === "string") {
+        bookmarkProducts = JSON.parse(bookmarkProducts);
+        bookmarkProducts = bookmarkProducts.filter(
+          (product) => Number(product.id) !== Number(productId)
+        );
+        e.target.dataset.action = "ADD";
+        e.target.textContent = "Add to Fav";
+        if (showBookmark) {
+          generateCardList(bookmarkProducts);
+        }
+      }
+    }
+    localStorage.setItem("Products", JSON.stringify(bookmarkProducts));
   }
 });
 
@@ -99,3 +155,14 @@ function filterHandler(data, tag = "skincare") {
   });
   return filterData;
 }
+
+bookmark.addEventListener("click", () => {
+  showBookmark = !showBookmark;
+  if (showBookmark) {
+    const bookmarkProducts = JSON.parse(localStorage.getItem("Products"));
+    generateCardList(bookmarkProducts);
+  } else {
+    data.push(...products);
+    generateCardList(data);
+  }
+});
